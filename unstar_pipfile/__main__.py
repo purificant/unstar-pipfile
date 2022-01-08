27d3b4fc794bc98de9a8fae14dd2289e5b413fea
purificant <purificant@users.noreply.github.com>
2022-01-08 18:41:03 +0000

    test installation

--- unstar_pipfile/__main__.py
+++ unstar_pipfile/__main__.py
@@ -1,4 +1,10 @@
 from .unstar_pipfile import unstar
 
 if __name__ == "__main__":
+    """When running as a module."""
+    unstar()
+
+
+def main():
+    """When running as entrypoint script."""
     unstar()
